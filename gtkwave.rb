60a52ce5d40650c427c0d5771c31b30a20a892f4
Andy Lin <andy.lin.2004@outlook.com>
2023-09-29 21:08:29 -0400

    fix some slight name errors

--- gtkwave.rb
+++ gtkwave.rb
@@ -16,7 +16,7 @@ class Gtkwave < Formula
 
   patch do
 	url "https://raw.githubusercontent.com/andylin2004/homebrew-gtkwave/main/macos_compat.diff"
-	sha256 "562cfb87a7a981c93bfba75389616bc625404cee310d9b72dcfefd7ba829f269"
+	sha256 "8e403c115c4e27834df1b679aa5e427d3ddee7d5467b77c61d925843a05f5d4c"
 	end
 
   def install
